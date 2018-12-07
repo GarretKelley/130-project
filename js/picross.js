07602bd9075ddf18e7d16ba56561bbb611ed5d92
David Parker <crazdave@gmail.com>
2018-12-06 23:12:53 -0800

    detect win condition

--- js/picross.js
+++ js/picross.js
@@ -40,19 +40,40 @@ var stopwatch = (function() {
 })();
 
 var gameBoard = {};
+
+function didWin() {
+    var $cells = $('.cell');
+    var IDs = Object.keys(gameBoard);
+    if (IDs.length !== $cells.length) {
+        return false;  // can't win if all cells aren't selected
+    }
+    for (var i = 0; i < IDs.length; i++) {
+        if ($('#' + IDs[i]).attr('data-val') != gameBoard[IDs[i]]) {
+            return false;
+        }
+    }
+    return true;
+}
     
 // turns any cell clicked on blue, and right-clicked to grey. 
-//also starts the timer and increments turns
-// starting point for determining correct / incorrect moves, etc.
+// starts the timer and increments turns
+// determines correct / incorrect moves, score
 $(function() {
     var $counter = $('#turnsCounter');
     var counter = 0;
     var $mistakes = $('#mistakesCounter');
     var mistakes = 0;
+    var numCells = $('.cell').length;
 
     function handleClick($clickedCell, cellValue, backgroundColor) {
         var id = $clickedCell.attr('id');
         var correctValue = $clickedCell.attr('data-val');
+
+        $clickedCell.css('background-color', backgroundColor);
+        if (!stopwatch.started) {
+            stopwatch.start('.stopwatch');
+        }
+
         if (gameBoard[id] !== cellValue) {
             counter += 1;
             $counter.text(counter);
@@ -60,11 +81,11 @@ $(function() {
                 mistakes += 1;
                 $mistakes.text(mistakes);
             }
-        }
-        gameBoard[id] = cellValue;
-        $clickedCell.css('background-color', backgroundColor);
-        if (!stopwatch.started) {
-            stopwatch.start('.stopwatch');
+            gameBoard[id] = cellValue;
+            // win condition
+            if (didWin()) {
+                alert('you win!');
+            }
         }
     }
 
