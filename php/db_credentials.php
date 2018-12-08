c2514fb7d7237faf1d23801802d7eb41de112597
David Parker <crazdave@gmail.com>
2018-12-07 19:14:05 -0800

    dont ignore this

--- php/db_credentials.php
+++ php/db_credentials.php
@@ -1,8 +1,8 @@
 <?php
 // update with server connection and name needed
 define("DB_Server", "localhost:3306");
-define("DB_User", "root");
-define("DB_Pass", "");
+define("DB_User", "csci130");
+define("DB_Pass", "123456");
 define("DB_Name", "130_project");
 
 ?>
